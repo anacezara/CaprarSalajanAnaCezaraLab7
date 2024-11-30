0e777a4cfd6694a1a4993b21c081d507942bd8ef
ANA-CEZARA CĂPRAR-SĂLĂJAN <ana.caprar@stud.ubbcluj.ro>
2024-11-30 18:48:45 +0200

    Laborator7

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace CaprarSalajanAnaCezaraLab7
+﻿using System;
+using CaprarSalajanAnaCezaraLab7.Data;
+using System.IO;
+
+namespace CaprarSalajanAnaCezaraLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database; 
+        public static ShoppingListDatabase Database 
+        {
+            get 
+            {
+                if (database == null) 
+                { 
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3")); 
+                } 
+                return database; 
+            } 
+        }
         public App()
         {
             InitializeComponent();
